80faf56b8e4471058d61c07b1c1ed9748bb04926
Yehor Makarenko <mak.yehor.knu@gmail.com>
2023-09-21 04:22:18 +0300

    FixedPointIteration ready

--- src/Lab1/FixedPointIteration.java
+++ src/Lab1/FixedPointIteration.java
@@ -25,18 +25,17 @@ public class FixedPointIteration {
     }
   };
 
-  private static final Function phi1 = new Function() {
+  private static final Function phi2 = new Function() {
     @Override
     public double getValue(double x) {
-      double a = (-(Math.pow(x, 4) + 48.16 * Math.pow(x, 2) + 108.08 * x + 70.76) / 10);
-      return Math.signum(a) * Math.pow(Math.abs(a), (double)1 / 3);
+      return -Math.pow(-10 * Math.pow(x, 3) - 48.16 * Math.pow(x, 2) - 108.08 * x - 70.76, (double)1 / 4);      
     }
   };
 
-  private static final Function phiDer1 = new Function() {
+  private static final Function phiDer2 = new Function() {
     @Override
     public double getValue(double x) {           
-      return -(4 * Math.pow(x, 3) + 96.32 * x + 108.08) / (3 * Math.pow(10, (double)1 / 3) * Math.pow(Math.pow(x, 4) + 48.16 * Math.pow(x, 2) + 108.08 * x + 70.76, (double)2 / 3));
+      return (30 * Math.pow(x, 2) + 96.32 * x + 108.08) / (4 * Math.pow(-10 * Math.pow(x, 3) - 48.16 * Math.pow(x, 2) - 108.08 * x - 70.76, (double)3 / 4));
     }
   };
 
@@ -46,8 +45,6 @@ public class FixedPointIteration {
   private static double left, right, x0, q, prevX, currX, newX;  
 
   public static void main(String[] args) {
-    mainPhi = phi1;
-    mainPhiDer = phiDer1;
     Scanner input = new Scanner(System.in);    
 
     System.out.println("Left bound: ");
@@ -62,6 +59,14 @@ public class FixedPointIteration {
       System.out.println("Wrong x0");
       return;
     }     
+
+    if (right > -1.4) {
+      mainPhi = phi1;
+      mainPhiDer = phiDer1;
+    } else {
+      mainPhi = phi2;
+      mainPhiDer = phiDer2;
+    }
 
     if (!checkFirstCondition() || !checkSecondCondition()) {
       System.out.println("Cannot use fixed-point iteration");
@@ -85,14 +90,14 @@ public class FixedPointIteration {
 
   private static boolean checkFirstCondition() {
     q = MinMaxAbsFunction.getMaxAbs(mainPhiDer, left, right);
-    System.out.println(q);
+    System.out.println("q: " + q);
     return q < 1;
   }
 
   private static boolean checkSecondCondition() {
     double delta = Math.max(x0 - left, right - x0);    
-    System.out.println(Math.abs(mainPhi.getValue(x0) - x0));
-    System.out.println((1 - q) * delta);
+    System.out.println("|phi(x0)-x0|: " + Math.abs(mainPhi.getValue(x0) - x0));
+    System.out.println("(1-q)*delta: " + (1 - q) * delta);
     return Math.abs(mainPhi.getValue(x0) - x0) <= (1 - q) * delta;
   }
 
